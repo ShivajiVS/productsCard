df8270ef07093109237ae41d33a9b55abd94aeaa
Kondeti Sivaji <shivajikondeti555@gmail.com>
2024-11-14 15:39:41 +0530

    dark & light mode with local storage

--- src/themeProvider.jsx
+++ src/themeProvider.jsx
@@ -1,17 +1,23 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const ThemeContext = createContext({
   theme: "light",
-  toggleTheme: () => {}, 
+  toggleTheme: () => {},
 });
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("light"); 
+  const savedTheme = localStorage.getItem("theme") || "light";
+  const [theme, setTheme] = useState(savedTheme);
 
   const toggleTheme = () => {
-    console.log("handler...");
-    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
   };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
